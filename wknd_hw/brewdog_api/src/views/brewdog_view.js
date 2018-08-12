a57137b074245966770da89303145e05990a4799
Adams1991 <adams121@hotmail.co.uk>
2018-08-12 17:39:55 +0100

    name and desc rendering

--- wknd_hw/brewdog_api/src/views/brewdog_view.js
+++ wknd_hw/brewdog_api/src/views/brewdog_view.js
@@ -14,13 +14,22 @@ BrewdogView.prototype.bindEvents = function () {
 BrewdogView.prototype.render = function (brewdogs) {
   this.container.innerHTML = '';
 
-  brewdogs.forEach((brewdog) => {
-    const beerName = document.createElement('h2');
-    beerName.textContent = brewdog.name
-    this.container.appendChild(beerName);
-  })
+    brewdogs.forEach((brewdog) => {
+      const beerName = this.createElement('h2', brewdog.name);
+      this.container.appendChild(beerName);
+
+      const beerDesc = this.createElement('p', brewdog.description)
+      this.container.appendChild(beerDesc);
 
 
+    })
+
+};
+
+BrewdogView.prototype.createElement = function (elementType, text) {
+  const element = document.createElement(elementType);
+  element.textContent = text;
+  return element;
 };
 
 module.exports = BrewdogView;

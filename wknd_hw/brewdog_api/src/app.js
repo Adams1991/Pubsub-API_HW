a7cbe4bda9552b83b1e4c3d2482c863a937794af
Adams1991 <adams121@hotmail.co.uk>
2018-08-13 08:59:37 +0100

    select by abv partially working but some abvs seem to be missing will look after review

--- wknd_hw/brewdog_api/src/app.js
+++ wknd_hw/brewdog_api/src/app.js
@@ -1,8 +1,14 @@
 const FormView = require('./views/form_view.js');
 const BrewdogView = require('./views/brewdog_view.js');
+const SelectView = require('./views/select_view.js');
 const Brewdogs = require('./models/brewdogs.js');
 
 document.addEventListener('DOMContentLoaded', () => {
+  const selectElement = document.querySelector('select#beer-select');
+  const selectView = new SelectView(selectElement);
+  selectView.bindEvents();
+
+
   const form = document.querySelector('#beer-form')
   const formView = new FormView(form);
   formView.bindEvents();
--- wknd_hw/brewdog_api/src/models/brewdogs.js
+++ wknd_hw/brewdog_api/src/models/brewdogs.js
@@ -11,11 +11,32 @@ Brewdogs.prototype.bindEvents = function () {
     this.getData(beer);
   });
 
+  PubSub.subscribe('SelectView:change', (event)  => {
+  const beer = event.detail;
+  this.getData(beer)
+})
+
   PubSub.subscribe('FormView:button-click', (event) => {
     this.getRandomData();
   });
 
+  this.getForSelectData();
 
+
+}
+
+
+Brewdogs.prototype.getForSelectData = function (beer_name) {
+  const url = `https://api.punkapi.com/v2/beers`;
+  const request = new Request(url);
+  request.get()
+    .then((data) => {
+      this.data = data
+      PubSub.publish('Brewdogs:select-data-loaded', this.data)
+    })
+    .catch((message) => {
+      console.error(message);
+    });
 }
 
 Brewdogs.prototype.getData = function (beer_name) {
